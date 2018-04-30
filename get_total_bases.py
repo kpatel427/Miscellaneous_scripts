ade0298f57ca7697c1638e80c48bbedbcf61a092
Khushbu Patel <kpatel427@gatech.edu>
2018-04-30 11:50:09 -0400

    upload

---
+++ get_total_bases.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python
+# Khushbu Patel | 04/30/2018
+# Calculates total number of bases in fasta file
+# Usage: ./get_total_bases.py 
+
+infile=""
+infile=raw_input("Enter the file name:") #takes file as user input from the command line
+f=open(infile,"r")
+sum = 0
+
+for line in f:
+	line = line.rstrip()	#quivalent of chomp in perl; removes new line characters
+	if line.startswith('>'):
+		continue		#if starts with header, do nothing
+	else:
+		chars = list(line)	#parse the str to list
+		sum += len(chars)	#count the list using len(); every iteration adds to the previous sum value
+		
+	
+print(sum)	#print the sum
