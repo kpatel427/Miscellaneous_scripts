dbebfd6867f2ddcf086469a9a81277e8e83cb54a
Khushbu Patel <kpatel427@gatech.edu>
2018-05-09 09:03:34 -0400

    update

--- hqSNP_diagnose.pl
+++ hqSNP_diagnose.pl
@@ -1,16 +1,17 @@
 #!/usr/bin/env perl  
 # Khushbu Patel 3-16-18
 # hqSNP diagnose script / QC lyve-SET/1.1.4f
-# Run this script in the folder where your bam files are present
+# Run this script from your main Lyve-SET folder
 
 use warnings;
 use strict;
 use Bio::SeqIO;
+use Data::Dumper;
 
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------#
 
-my $usage = "\nUsage: perl /path/hqSNP_diagnose.pl ./reference/reference.fasta\n\nPlease set the working directory to the main Lyve-SET folder and supply a reference genome!\n\nMake sure you have loaded modules - samtools/1.4.1 and perl/5.16.1-MT!\n";
+my $usage = "\nUsage: ./hqSNP_diagnose.pl ./reference/reference.fasta\n\nPlease set the working directory to the main Lyve-SET folder and supply a reference genome!\n\nMake sure you have loaded modules - samtools/1.4.1 and perl/5.16.1-MT!\n";
 my $reference = $ARGV[0] or die $usage;
 
                
@@ -29,6 +30,10 @@ my $reference = $ARGV[0] or die $usage;
                my $proper_paired;
                my $perc_proper_paired;
                my $total_ref_bases;
+			   my $str5;
+			   my $perc_reference_coverage;
+			   my $average_coverage;
+			   my $vcf_out;
                
 
 
@@ -75,7 +80,7 @@ sub gsize()  #Subroutine to calculate size of Genome
                }
 
                $total_ref_bases = $length;
-               print "Genome Size: $total_ref_bases\n";
+               print "\nGenome Size: $total_ref_bases\n";
 
 
                }
@@ -87,8 +92,30 @@ gsize();
 
 chdir("./bam/");
 
-my @files = glob("*.bam");
+ #-------------------------------------------------------------------------------------------------------------------------------#
+               sub masking;      #function declaration to prevent error: "too early to prototype"
+               
+               
+               
+               print "\n\n\n================================================== MASKING INFORMATION ==============================================================\n";
+               
+               print "\n\n#......Overall masking.....#\n";
+               print "-------------------------------------\n";
+               
+               masking($out_aln_path);              #calling subroutine masking and passing out.aln file as a parameter
+               
+               print "\n\n#......Masking in informative sites.....#\n";
+               print "-------------------------------------\n";
 
+               masking($out_info_path);             #calling subroutine masking and passing out.info file as a parameter
+
+
+			   
+			   
+#-------------------------------------------------------------------------------------------------------------------------------#			   
+			   
+
+my @files = glob("*.bam");
 
 foreach my $i(@files)
 {
@@ -96,9 +123,9 @@ foreach my $i(@files)
                
                ($basename = $i) =~ s/.fastq*.+//;            #Get the basename of files
 			   
-          
+		       print ("\n------------------------------------------------------------------------------------------------------------------------------------------------------\n");
                print "\n\n\n---> Processing file: $basename...\n";
-               print "\n\n=============================== QC on BAM File =========================\n";
+               print ("\n\n=============================== QC on BAM File =========================\n");
                
                $total_mapped_reads = `samtools view -c -F 4 $i`; #Fetching total number of mapped reads  #NOTE: saving system() output to perl variable, returns 0; it sets the exit status to perl variable; Hence use ``
                $mapped_reads = `samtools view -q 30 $i | wc -l`;  #Fetching number of mapped reads with quality higher than 30
@@ -111,23 +138,19 @@ foreach my $i(@files)
                my $line_count= `zcat $file | wc -l`;   #gettting total number of lines in fastq files
                $tot_reads = $line_count/4;          #Dividing total number of lines by 4, to get total number of reads
                $not_mapped = $tot_reads - $total_mapped_reads; # Calculating number of reads that are not mapped
-               $perc_mapped_reads = ($total_mapped_reads/$tot_reads) * 100; #Calculating percentage of mapped reads
-               $perc_mapped_reads_gt_30 = ($mapped_reads/$tot_reads) * 100; #Calculating percentage of mapped reads > Q30
-               
-               
-               print "\n#......Mapped reads....#\n";
-               print "-------------------------------------\n";
-               print "total number of reads in File -> $basename: $tot_reads\n";  # Printing total number of reads
+               $perc_mapped_reads = ($total_mapped_reads/$total_mapped_reads) * 100; #Calculating percentage of mapped reads
+               $perc_mapped_reads_gt_30 = ($mapped_reads/$total_mapped_reads) * 100; #Calculating percentage of mapped reads > Q30
+			   
+			   
+			 
+               print "\nTotal number of reads in File -> $basename: $tot_reads\n";  # Printing total number of reads
                print "Total number of mapped reads: $total_mapped_reads";
-               print "\tNumber of Reads Mapped > Q30: $mapped_reads";
-               print "\tNumber of Reads mapped < Q30: $mapped_reads_lt_30\n";
+               print "Number of Reads Mapped > Q30: $mapped_reads";
+               print "Number of Reads mapped < Q30: $mapped_reads_lt_30\n";
                print "Number of Reads not mapped: $not_mapped\n";
                print "Percent mapped reads: $perc_mapped_reads\n";
                print "Percent mapped reads > Q30: $perc_mapped_reads_gt_30\n";
-               
-               
-               print "\n#......Read Pairing.....#\n";
-               print "-------------------------------------\n";
+              
 
                my $str1 = `samtools stats  $i | head -37 | grep 'reads properly paired:'`;
                $str1 =~s/^SN\s+//;        #Formatting output
@@ -155,35 +178,26 @@ foreach my $i(@files)
                
                
                print "Percentage of properly paired reads = $perc_proper_paired\n";
+              
                
-               print "\n#......Insert Size.....#";
-               print "\n-------------------------------------\n";
-               
-               my $str5 = `samtools stats $i | head -37 | grep 'insert size average:'`;
+               $str5 = `samtools stats $i | head -37 | grep 'insert size average:'`;
                $str5 =~s/^SN\s+//;                        #Formatting output
                print $str5;
                
-
-
-               print "\n\n#......Reference Coverage.....#\n";
-               print "-------------------------------------\n";
                
                my $baseslt20 = `samtools depth $i| awk '( (\$3 >= 0) && (\$3 <= 19) )'| wc -l`;
                print "Number of bases with mapping coverage less than 20: $baseslt20";
 
                
-               my $perc_reference_coverage = ((($total_ref_bases - $baseslt20)/$total_ref_bases)*100);
-               print "Percent reference coverage = $perc_reference_coverage";
+               $perc_reference_coverage = ((($total_ref_bases - $baseslt20)/$total_ref_bases)*100);
+               print "Percent reference coverage = $perc_reference_coverage\n";
                
-               
-
-               print "\n\n#......Average Coverage.....#\n";
-               print "-------------------------------------\n";
+      
                
                my $tot_coverage_bases = `samtools depth $i | cut -f 3| awk '{sum += \$_} END {print sum}'`;  # Total coverage of all bases
-               print "Total coverage of bases = $tot_coverage_bases";
+               print "Total coverage of bases (based off 20x at each position) = $tot_coverage_bases";
                
-               my $average_coverage = $tot_coverage_bases/$total_ref_bases;
+               $average_coverage = $tot_coverage_bases/$total_ref_bases;
                print "Average Coverage = $average_coverage";
 
 
@@ -191,37 +205,24 @@ foreach my $i(@files)
                my $vcf_filename= "$vcf_path"."/".$basename."*.gz";
                
                
-               print "Filters applied for sites that do not pass -
+               print("Filters applied for sites that do not pass -
 a. DP20 = Depth is less than 20, the user-set coverage threshold
 b. RF0.95 = Reference variant consensus is less than 0.95, the user-set threshold
 c. AF 0.95 = Allele variant consensus is less than 0.95, the user-set threshold
 d. isIndel = Indels are not used for analysis in Lyve-SET
 e. masked = This site was masked using a bed file or other means
 f. str10 = Less than 10% or more than 90% of variant supporting reads on one strand
-g. indelError = Likey artifcat due to indel reads at this position\n\n";
+g. indelError = Likey artifcat due to indel reads at this position\n\n");
                
                
-               system("zcat $vcf_filename | grep -v '^#' | cut -f 7 | sort | uniq -c");
+				$vcf_out =`zcat $vcf_filename | grep -v '^#' | cut -f 7 | sort | uniq -c`;
+				print("$vcf_out");
+				
+
+               }
 
 
-               } 
-
-
-               sub masking;      #function declaration to prevent error: "too early to prototype"
-               
-               
-               
-               print "\n\n\n=============================== MASKING INFORMATION =========================\n";
-               
-               print "\n\n#......Overall masking.....#\n";
-               print "-------------------------------------\n";
-               
-               masking($out_aln_path);                             #calling subroutine masking and passing out.aln file as a parameter
-               
-               print "\n\n#......Masking in informative sites.....#\n";
-               print "-------------------------------------\n";
-
-               masking($out_info_path);             #calling subroutine masking and passing out.info file as a parameter
+			  
                
                
                
