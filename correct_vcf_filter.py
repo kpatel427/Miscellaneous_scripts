fecac0364bc765048a2c817cf6d300b6e3122d33
Khushbu Patel <kpatel427@gatech.edu>
2018-05-18 12:12:37 -0400

    update

--- correct_vcf_filter.py
+++ correct_vcf_filter.py
@@ -2,50 +2,64 @@
 # Khushbu Patel | 05/15/2018
 # Corrects the filter and Calculates number of bases falsely assigned incorrect filter when the bases pass the consensus and coverage. Prints out a new vcf with filters corrected. 
 # Usage: ./correct_vcf_filter.py inputfile.vcf
-# Output will be another vcf file, named correct_vcf.vcf; This script will not overwrite the original VCF
+# Output will be two files a corrected vcf file and a text file, the text file contains all those hits for which the filter was changed. Please note, this script will not overwrite the original VCF.
 
 import sys
+import os
+import subprocess
 
 infile= sys.argv[1]	# Takes the file name as a command line argument
 f=open(infile,"r")
 
-
+basename = os.path.basename(infile)
+outfile_vcf = basename + "_corrected.vcf"
+outfile_txt = basename + "_corrected.txt"
 temp = []
-count =0
+count = 0
 out = []
 ID = ''
+old_filter = 0
 
-with open('correct_vcf.vcf', 'a') as f1:
-	for line in f:
-		line =line.rstrip()
-		if line.startswith('#'):
-			ID = line					# Printing the headers
-			print >>f1, ID
+
+with open(outfile_vcf, 'a') as f1:
+	with open(outfile_txt, 'a') as f2:
+		print >>f2, "CHROM\tPOS\tNEW_FILTER\tDEPTH\tFREQ\tPOS\tNEW_FILTER\tDEPTH\tFREQ\tOLD_FILTER"
+		for line in f:
+			line =line.rstrip()
+			if line.startswith('#'):
+				ID = line					# Printing the headers
+				print >>f1, ID
 		
-		else:
-			array = line.split()
-			temp = array[9].split(':')
+			else:
+				array = line.split()
+				temp = array[9].split(':')
 			
-			if(array[6] != "PASS"):
-				if(len(temp)> 6):
-					temp[6] = temp[6].replace('%','')
-					if(temp[3] >= 20 and float(temp[6]) < 5.0):		# if coverage and consensus both meet, change the filter to PASS
-						#print(line, "--Incorrect Filter!")
-						array[6] = "PASS"
+				if(array[6] != "PASS"):
+					if(len(temp)> 6):
+						old_filter = array[6]		# Stores old filter
+						temp[6] = temp[6].replace('%','')
+						if(temp[3] >= 20 and float(temp[6]) < 5.0):		# if coverage and consensus both meet, change the filter to PASS
+							#print(line, "--Incorrect Filter!")			# Sanity check!
+							array[6] = "PASS"
+							count += 1					# Count the number of sites that have been assigned wrong filter
+							out = '\t'.join(array)
+							print >>f1, out
+							str = array[0]+'\t'+array[1]+'\t'+array[6]+'\t'+temp[3]+'\t'+temp[6]+'\t'+old_filter
+							print >>f2, str
+						
+						
+						else:							# if allele frequency is greater than 5% or coverage does not meet; keep the filter
+							out = '\t'.join(array)
+							print >>f1, out
+	
+					else:									# else - print everything as is
 						out = '\t'.join(array)
-						count += 1					# Count the number of sites that have been assigned wrong filter
 						print >>f1, out
-					else:							# if allele frequency is greater than 5% or coverage does not meet; keep the filter
-						out = '\t'.join(array)
-						print >>f1, out
-
+				
 				else:									# else - print everything as is
 					out = '\t'.join(array)
 					print >>f1, out
-				
-			else:									# else - print everything as is
-				out = '\t'.join(array)
-				print >>f1, out
-				
-	print("Total sites lost due to incorrect filter %d" % count)	
+	
+	
+	
 		
