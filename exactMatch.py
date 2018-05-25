fe98a5c4580c80b18bd6b9bf7de8d14ef0dd674c
Khushbu Patel <kpatel427@gatech.edu>
2018-05-25 13:40:03 -0400

    upload

---
+++ exactMatch.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python
+# Khushbu Patel | 05/25/2018
+# enter the pattern (p) and Alignment (t) to search from; returns the positions where there were exact matches
+# Usage: ./exactmatch.py 
+
+def exactMatch(p,t):
+	occurences = []
+	for i in range(len(t) - len(p)+1):
+		match = True
+		for j in range(len(p)):
+			if(t[i+j] != p[j]):
+				match = False
+				break
+		if match:
+			occurences.append(i)
+	return occurences
+	
+result = exactMatch('AT','ATTGCAGTCGATCGATTGCGATAGA')
+print(result)
