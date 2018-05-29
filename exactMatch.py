45052ed6ef55031c0f829d3e45656ef259e9019b
Khushbu Patel <kpatel427@gatech.edu>
2018-05-29 08:49:13 -0400

    update

--- exactMatch.py
+++ exactMatch.py
@@ -3,17 +3,30 @@
 # enter the pattern (p) and Alignment (t) to search from; returns the positions where exact matches occur
 # Usage: ./exactmatch.py 
 
+
+def reverseComplement(s):
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N'}
+    t = ''
+    for base in s:
+        t = complement[base] + t
+    return t
+
+
+
 def exactMatch(p,t):
 	occurences = []
+	reverse = reverseComplement(p)
 	for i in range(len(t) - len(p)+1):
 		match = True
 		for j in range(len(p)):
 			if(t[i+j] != p[j]):
 				match = False
-				break
+				if(t[i+j] != reverse[j]):
+					match = False
+					break
+				else:
+					match = True
+					
 		if match:
 			occurences.append(i)
 	return occurences
-	
-result = exactMatch('AT','ATTGCAGTCGATCGATTGCGATAGA')
-print(result)
